f1d38808110c87299d07680387c3da01035298b6
myselfims <shaikhimran7585@gmail.com>
2023-09-14 13:56:33 +0530

    host changes

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l7cq*q539-ecd25mx899k-ui$ra6imzcm^psgqip_i$!##sh-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://task-management-system-django.onrender.com/']
 
