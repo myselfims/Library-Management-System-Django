d435a0a4386f7f9070d6257c2f08b2255823ce71
myselfims <shaikhimran7585@gmail.com>
2023-09-14 13:59:28 +0530

    host changes

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l7cq*q539-ecd25mx899k-ui$ra6imzcm^psgqip_i$!##sh-0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://task-management-system-django.onrender.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
